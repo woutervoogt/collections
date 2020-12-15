f8c7d7c333fd593ce3cf72f88ae4c54863633e9d
Wouter Voogt <woutervoogt@gmail.com>
2020-12-15 15:21:19 +0100

    add a simple navbar

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -1,11 +1,36 @@
 <header>
-    <nav>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/about/culture">Culture</a></li>
-            <li><a href="/contact">Contact</a></li>
-        </ul>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Collections</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link " aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " aria-current="page" href="/verzamelingen">Verzamelingen</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " aria-current="page" href="/verzameling_aanmaken">Verzameling
+                            aanmaken</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " aria-current="page" href="/verzameling_aanmaken">Account</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/inloggen">Inloggen</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/registreren">Registreren</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
     </nav>
 
 </header>
