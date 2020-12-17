746333d07f447f98e12fe0ea6d5f960d3e040ced
Wouter Voogt <woutervoogt@gmail.com>
2020-12-17 13:26:10 +0100

    create a basic homepage

--- app/views/index.view.php
+++ app/views/index.view.php
@@ -1,12 +1,34 @@
 <?php require 'partials/head.php'; ?>
 <?php require 'partials/header.php'; ?>
 
-<h1>
-    Home Page
-</h1>
+<main>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 home-left-side p-3 p-md-5 bg-primary">
+                <div class="container p-0 p-md-3 p-lg-5">
+                    <img src="/public/images/homepage-image.jpg" class="img-fluid" alt="...">
+                </div>
 
 
+            </div>
+            <div class="col-md-6 align-self-center">
+                <section class="text-center w-75 mx-auto">
+                    <h1>
+                        Collections
+                    </h1>
+                    <hr>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi fuga repellendus temporibus
+                        vitae similique incidunt fugiat, aperiam dolore voluptas labore reiciendis ut perferendis autem
+                        cupiditate, a culpa qui officiis consectetur.
+                    </p>
+                    <a href="/verzameling_aanmaken">Maak je eigen verzameling aan</a>
+                </section>
+            </div>
+        </div>
 
+    </div>
+</main>
 
 
 <?php require 'partials/footer.php';
