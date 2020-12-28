6f9cba22d1e5f0b477a2e73fc01d565c5fa80c31
Wouter Voogt <woutervoogt@gmail.com>
2020-12-28 13:57:20 +0100

    setup database migrations

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -2,6 +2,8 @@
 
 namespace App\Core\Database;
 
+use App\Core\App;
+
 class QueryBuilder
 {
     protected $pdo;
@@ -9,6 +11,20 @@ class QueryBuilder
     public function __construct(\PDO $pdo)
     {
         $this->pdo = $pdo;
+    }
+
+    public static function query($query, $executeString = array())
+    {
+        $dbh = App::get('database')->pdo;
+
+        try {
+            $stmt = $dbh->prepare($query);
+            $stmt->execute($executeString);
+        } catch (\PDOException $e) {
+            var_dump($e->getMessage());
+        }
+
+        return $stmt;
     }
 
     public function selectAll($table)
@@ -19,18 +35,23 @@ class QueryBuilder
         return $statement->fetchAll(\PDO::FETCH_CLASS);
     }
 
-    public function insert($table, $parameters)
+    /**
+     * Insert data into the database
+     * @param $table string: the table to insert into
+     * @param $data array: a associative array with columns and values
+     */
+    public function insert($table, $data)
     {
         $sql = sprintf(
             'insert into %s (%s) values (%s)',
             $table,
-            implode(', ', array_keys($parameters)),
-            ':' . implode(', :', array_keys($parameters))
+            implode(', ', array_keys($data)),
+            ':' . implode(', :', array_keys($data))
         );
 
         try {
             $statement = $this->pdo->prepare($sql);
-            $statement->execute($parameters);
+            $statement->execute($data);
         } catch (\Exception $e) {
             die($e->getMessage());
         }
